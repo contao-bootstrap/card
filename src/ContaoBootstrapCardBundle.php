be70d62e0420cc7665ccf077027f31bb4b816127
David Molineus <david.molineus@netzmacht.de>
2017-09-26 11:44:18 +0200

    Add bundle and contao manager class.

---
+++ src/ContaoBootstrapCardBundle.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Contao Bootstrap card.
+ *
+ * @package    contao-bootstrap
+ * @subpackage Card
+ * @author     David Molineus <david.molineus@netzmacht.de>
+ * @copyright  2017 netzmacht David Molineus. All rights reserved.
+ * @license    LGPL 3.0
+ * @filesource
+ */
+
+namespace ContaoBootstrap\Card;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class ContaoBootstrapCardBundle.
+ *
+ * @package ContaoBootstrap\Card
+ */
+class ContaoBootstrapCardBundle extends Bundle
+{
+
+}
---
+++ src/ContaoManager/Plugin.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * Contao Bootstrap card.
+ *
+ * @package    contao-bootstrap
+ * @subpackage Card
+ * @author     David Molineus <david.molineus@netzmacht.de>
+ * @copyright  2017 netzmacht David Molineus. All rights reserved.
+ * @license    LGPL 3.0
+ * @filesource
+ */
+
+namespace ContaoBootstrap\Card\ContaoManager;
+
+use Contao\CoreBundle\ContaoCoreBundle;
+use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
+use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
+use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+use ContaoBootstrap\Card\ContaoBootstrapCardBundle;
+use ContaoBootstrap\Core\ContaoBootstrapCoreBundle;
+
+/**
+ * Contao Manager plugin.
+ */
+class Plugin implements BundlePluginInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getBundles(ParserInterface $parser)
+    {
+        return [
+            BundleConfig::create(ContaoBootstrapCardBundle::class)
+                ->setLoadAfter([ContaoCoreBundle::class, ContaoBootstrapCoreBundle::class])
+        ];
+    }
+}
